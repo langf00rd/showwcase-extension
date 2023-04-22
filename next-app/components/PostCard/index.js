923997c315765091c10352b82347038175aab91d
Langford Kwabena <langfordquarshie21@gmail.com>
2023-04-22 13:39:02 +0000

    tweaks

--- next-app/components/PostCard/index.js
+++ next-app/components/PostCard/index.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import { BsFillBookmarkFill } from 'react-icons/bs'
 import { FiMoreVertical, FiX } from 'react-icons/fi'
+import { AiFillEye } from 'react-icons/ai'
 import { HiOutlineExternalLink } from 'react-icons/hi'
 import formatDate from "../../utils/formatDate.util";
 import formatNumber from "../../utils/formatNumber.util";
@@ -15,14 +16,20 @@ export default function PostCard({ title, dateTime, readTime, image, summary, vi
         if (showPostInfo) return <div className="bg-[#101827e8] w-screen h-screen overflow-y-scroll fixed left-0 top-0 z-10 flex flex-col items-center justify-center">
             <FiX onClick={() => setShowPostInfo(false)} className="cursor-pointer hover:text-brand transition-all my-10" size={50} />
             <div className="post-info-modal p-10 rounded-xl rounded-b-none h-full overflow-scroll w-full max-w-3xl border border-gray-700 bg-gray-800">
-                <h1 className="text-3xl font-bold">{title}</h1>
-                <p className="text-gray-400 my-2">{formatDate(dateTime)} • <span>{readTime}</span> • <span>{formatNumber(views)} views</span></p>
-                {image ? <img loading="lazy" src={image} className="w-full my-5 rounded-xl bg-gray-800" alt={"blog post image of" + title} />
-                    : <div className="w-full h-[170px] object-cover rounded-xl bg-gray-800" />}
+                <h1 className="text-3xl font-bold mb-2">{title}</h1>
+                <div className="text-gray-400 flex items-center gap-1">
+                    <p>{formatDate(dateTime)} • </p>
+                    <p>{readTime} • </p>
+                    <div className="flex items-center gap-1">
+                        <AiFillEye /> {formatNumber(views)} views
+                    </div>
+                </div>
+                {image ? <img loading="lazy" src={image} className="w-full my-5 rounded-xl bg-gray-700" alt={"blog post image of " + title} />
+                    : <div className="w-full my-5 rounded-xl bg-gray-700 h-[300px]" />}
                 <div className="my-5 gap-5 flex items-center justify-end">
                     <div className="flex items-center gap-2 text-gray-400 cursor-pointer hover:text-brand transition-all">
                         <BsFillBookmarkFill />
-                        <p>Add to bookmark</p>
+                        <p>Add to bookmarks</p>
                     </div>
                     <Link passHref href={link}>
                         <a href={link} target='_blank' rel='noreferrer'>
@@ -33,7 +40,10 @@ export default function PostCard({ title, dateTime, readTime, image, summary, vi
                         </a>
                     </Link>
                 </div>
-                <p>{summary}</p>
+                {summary && <>
+                    <label className={styles.label}>Summary</label>
+                    <p className="text-gray-300">{summary}</p>
+                </>}
             </div>
         </div>
 
@@ -54,11 +64,17 @@ export default function PostCard({ title, dateTime, readTime, image, summary, vi
             <div className="bg-gray-900 p-5 select-none rounded-xl relative border border-gray-800 hover:border-gray-600 cursor-pointer transition-all flex flex-col justify-between">
                 <div className="mb-3 flex flex-col gap-3">
                     <h3 className='font-medium w-[200px] text-[20px] max-lines-2'>{title}</h3>
-                    <p className="text-sm text-gray-400">{formatDate(dateTime)} • <span>{readTime}</span> • <span>{formatNumber(views)} views</span></p>
+                    <div className="text-sm text-gray-400 flex items-center gap-1">
+                        <p>{formatDate(dateTime)} • </p>
+                        <p>{readTime} • </p>
+                        <div className="flex items-center gap-1">
+                            <AiFillEye /> {formatNumber(views)} views
+                        </div>
+                    </div>
                     {!showMenu ? <FiMoreVertical onClick={() => setShowMenu(!showMenu)} className="hover:bg-gray-800 py-1 rounded-md absolute right-3 transition-all cursor-pointer" size={25} />
                         : <FiX onClick={() => setShowMenu(!showMenu)} className="hover:bg-gray-800 py-1 rounded-md absolute right-3 transition-all cursor-pointer" size={25} />}
                 </div>
-                {image ? <img onClick={() => setShowPostInfo(true)} loading="lazy" src={image} className="w-full h-[170px] object-cover rounded-xl bg-gray-800" alt={"blog post image of" + title} />
+                {image ? <img onClick={() => setShowPostInfo(true)} loading="lazy" src={image} className="w-full h-[170px] object-cover rounded-xl bg-gray-800" alt={"blog post image of " + title} />
                     : <div onClick={() => setShowPostInfo(true)} className="w-full h-[170px] object-cover rounded-xl bg-gray-800" />}
             </div>
         </li>
@@ -66,6 +82,7 @@ export default function PostCard({ title, dateTime, readTime, image, summary, vi
 }
 
 const styles = {
+    label: "text-gray-500 font-bold mb-1",
     iconLink: 'flex items-center whitespace-nowrap gap-1 transition-all hover:text-brand cursor-pointer',
     button: `w-max py-2 px-3 rounded-xl cursor-pointer flex items-center gap-2 bg-white transition-all hover:opacity-80 select-none text-gray-800`,
 }
