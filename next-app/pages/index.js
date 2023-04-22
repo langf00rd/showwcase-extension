c8ddc3a4ec55adde4509f2a2e950a89e9d4529cc
Langford Kwabena <langfordquarshie21@gmail.com>
2023-04-22 15:08:03 +0000

    update

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 import { useState, useEffect } from "react"
 import { HiPencil } from "react-icons/hi"
