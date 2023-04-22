e364195dda71539cb4ba1adfd4ab5f71e3e90fd7
Langford Kwabena <langfordquarshie21@gmail.com>
2023-04-22 12:29:46 +0000

    fix(post card layout): truncated title and changed font sizes

--- next-app/components/PostCard/index.js
+++ next-app/components/PostCard/index.js
@@ -6,6 +6,7 @@ import { FiMoreVertical, FiX } from 'react-icons/fi'
 export default function PostCard({ title, dateTime, readTime, image }) {
     const [showMenu, setShowMenu] = useState(false);
 
+
     function formatDate(dateStr) {
         const date = new Date(dateStr);
         const options = { month: "short", day: "numeric" };
@@ -13,42 +14,24 @@ export default function PostCard({ title, dateTime, readTime, image }) {
     }
 
     return (
-        <li className="bg-gray-900 p-5 select-none rounded-xl relative border border-gray-800 hover:border-gray-600 cursor-pointer transition-all">
-            {showMenu && (
-                <div className="border border-gray-800 p-5 rounded-xl absolute bg-gray-900 right-5 z-10 top-[50px]">
-                    <ul className="text-gray-400 flex flex-col gap-3">
-                        <li className={styles.iconLink}>
-                            <BsFillBookmarkFill />
-                            <p>Add to bookmarks</p>
-                        </li>
-                    </ul>
-                </div>
-            )}
+        <li className="bg-gray-900 p-5 select-none rounded-xl relative border border-gray-800 hover:border-gray-600 cursor-pointer transition-all flex flex-col justify-between">
+            {showMenu && <div className="border border-gray-800 p-5 rounded-xl absolute bg-gray-900 right-5 z-10 top-[50px]">
+                <ul className="text-gray-400 flex flex-col gap-3">
+                    <li className={styles.iconLink}>
+                        <BsFillBookmarkFill />
+                        <p>Add to bookmarks</p>
+                    </li>
+                </ul>
+            </div>}
             <div className="mb-3 flex flex-col gap-3">
-                <h3 className={`font-medium text-xl`}>{title}</h3>
+                <h3 className={`font-medium w-[200px] text-[20px] max-lines-2 ${title.length > 45 && "text-ellipsis"}`}>{title}</h3>
                 <p className="text-gray-400">
                     {formatDate(dateTime)} • <span>{readTime}</span>
                 </p>
-                {!showMenu ? (
-                    <FiMoreVertical
-                        onClick={() => setShowMenu(!showMenu)}
-                        className="hover:bg-gray-800 py-1 rounded-md absolute right-3 transition-all cursor-pointer"
-                        size={25}
-                    />
-                ) : (
-                    <FiX
-                        onClick={() => setShowMenu(!showMenu)}
-                        className="hover:bg-gray-800 py-1 rounded-md absolute right-3 transition-all cursor-pointer"
-                        size={25}
-                    />
-                )}
+                {!showMenu ? <FiMoreVertical onClick={() => setShowMenu(!showMenu)} className="hover:bg-gray-800 py-1 rounded-md absolute right-3 transition-all cursor-pointer" size={25} />
+                    : <FiX onClick={() => setShowMenu(!showMenu)} className="hover:bg-gray-800 py-1 rounded-md absolute right-3 transition-all cursor-pointer" size={25} />}
             </div>
-            <img
-                loading="lazy"
-                src={image}
-                className="w-full rounded-xl"
-                alt={"blog post image of" + title}
-            />
+            <img loading="lazy" src={image} className="w-full h-[170px] object-cover rounded-xl" alt={"blog post image of" + title} />
         </li>
     );
 }
